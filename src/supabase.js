58ed8987b74169980493227b9cfba2f32b641eee
fraber0 <bernyfreddi@gmail.com>
2026-04-24 11:24:30 +0200

    v. 0.2

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://eaypzgtzfhhcaendwarr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVheXB6Z3R6ZmhoY2FlbmR3YXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODc0NDAsImV4cCI6MjA5MjM2MzQ0MH0.lAuFX7rvr7dOBJYz_4_-FfHNWd9jp3KpTXkT4DUBXyg'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL 
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
